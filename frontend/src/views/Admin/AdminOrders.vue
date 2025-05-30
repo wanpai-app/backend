c7e7f98cfa21c1e605e33445fdf4cc0703d31c07
pcyyura <cyp.yura@gmail.com>
2025-05-30 23:03:10 +0800

    chore: remove frontend folder in preparation for backend repo split

--- frontend/src/views/Admin/AdminOrders.vue
+++
@@ -1,3 +0,0 @@
-<template>
-  <h2>這是後台訂單頁面</h2>
-</template>
--- frontend/src/views/Admin/EditProduct.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <h2 class="text-2xl">新增商品</h2>
-  <ProductForm />
-</template>
-
-<script setup>
-import ProductForm from '@/components/ProductForm.vue'
-</script>
