2e0d8ca2c7a8cb236e3cbf270979555f5ae8c612
kenyykd <117060245+kenyykd@users.noreply.github.com>
2025-06-05 22:39:02 +0800

    fix : delete api clientReturn

--- src/routes/ecpayRoutes.js
+++ src/routes/ecpayRoutes.js
@@ -1,19 +1,10 @@
 const express = require('express');
 const router = express.Router();
-<<<<<<< HEAD
-const { createOrder, handleReturn, clientReturn } = require('../controllers/ecpayController');
-router.post('/create-order', createOrder);
-
-router.post('/return', handleReturn);
-
-router.get('/clientReturn', clientReturn);
-=======
 const { createOrder, handleReturn } = require('../controllers/ecpayController');
 router.post('/create-order', createOrder);
 
 router.post('/handleReturn', handleReturn);
 
 
->>>>>>> d08ccc1 (feat: add ngrok file)
 
 module.exports = router;
