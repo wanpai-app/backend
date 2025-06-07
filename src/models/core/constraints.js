dc9d158239731bdec95b8bd76af83376e56ed91f
Hsinyun <tsuua040506@gmail.com>
2025-06-07 17:06:53 +0800

    chore: format files with Prettier

--- src/models/core/constraints.js
+++ src/models/core/constraints.js
@@ -1,10 +1,10 @@
-const { unique } = require("drizzle-orm/pg-core");
+const { unique } = require('drizzle-orm/pg-core');
 
 function fkTo(table, opts = {}) {
   return (col) =>
     col.references(() => table.id, {
-      onDelete: opts.onDelete || "set null",
-      onUpdate: opts.onUpdate || "cascade",
+      onDelete: opts.onDelete || 'set null',
+      onUpdate: opts.onUpdate || 'cascade',
     });
 }
 
