0e52162d0ccc0eb189279bb1204eee6b500db662
Hsinyun <tsuua040506@gmail.com>
2025-06-08 18:08:56 +0800

    chore: delete redundant line

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const express = require('express');
 const cors = require('cors');
 const productRoutes = require('./src/routes/productRoutes');
 const userRoutes = require('./src/routes/userRoutes');
-const productRoutes = require('./src/routes/productRoutes');
 const ecpayRoutes = require('./src/routes/ecpayRoutes');
 const orderRoutes = require('./src/routes/orderRoutes');
 
