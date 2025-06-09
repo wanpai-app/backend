b0f6e853722224530646506db319facceec63f45
pcyyura <cyp.yura@gmail.com>
2025-06-09 16:00:41 +0800

    feat: add update order status in ecpayController

--- src/controllers/orderController.js
+++ src/controllers/orderController.js
@@ -2,21 +2,12 @@ require('express');
 const db = require('../configs/db');
 const { ordersTable } = require('../models/orderSchema');
 const { eq } = require('drizzle-orm');
-const { nanoid } = require('nanoid');
-
-const generateOrderId = () => {
-  const now = new Date();
-  const yyyyMMdd = now.toISOString().slice(0, 10).replace(/-/g, ''); // '20250609'
-  const randomPart = nanoid(6);
-  return `OD-${yyyyMMdd}-${randomPart}`;
-};
 
 const createOrder = async (req, res) => {
   try {
     const [insertedOrder] = await db
       .insert(ordersTable)
       .values({
-        orderNumber: generateOrderId(),
         ...req.body,
         createdAt: new Date(),
         updatedAt: new Date(),
@@ -67,8 +58,12 @@ const softDeleteOrder = async (req, res) => {
   if (isNaN(id)) return res.status(400).json({ error: '無效的ID' });
 
   try {
-    await db.update(ordersTable).set({ isDeleted: true }).where(eq(ordersTable.id, id));
-    res.json({ message: '訂單已軟刪除' });
+    const [deleted] = await db
+      .update(ordersTable)
+      .set({ isDeleted: true })
+      .where(eq(ordersTable.id, id))
+      .returning();
+    res.status(201).json([deleted]);
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
