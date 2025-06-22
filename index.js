992bb8e935b5c7f4a3c55543bbd77825ae194ea3
kenyykd <117060245+kenyykd@users.noreply.github.com>
2025-06-22 12:06:24 +0800

    fix: add express.urlencoded middleware to parse ECPay form data

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ const tagsRoutes = require('./src/routes/tagsRoutes');
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use('/api', productRoutes);
 app.use('/api', require('./src/routes/orderRoutes'));
