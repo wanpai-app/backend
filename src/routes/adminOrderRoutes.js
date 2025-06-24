0e580aa5b1484806f860aa8b1cab21386cdc7951
pcyyura <cyp.yura@gmail.com>
2025-06-24 21:23:07 +0800

    fix: admin order api routes

--- src/routes/adminOrderRoutes.js
+++ src/routes/adminOrderRoutes.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const {
   createOrder,
   getAdminOrders,
-  getAllOrders,
   softDeleteOrder,
   updateOrder,
 } = require('../controllers/adminOrderController');
@@ -14,7 +13,6 @@ const { orderSchema } = require('../validators/orderValidator');
 
 router.get('/admin/orders', authenticateToken, isAdmin, getAdminOrders);
 router.put('/admin/orders/:id', authenticateToken, isAdmin, updateOrder);
-router.get('/admin/orders/all', authenticateToken, isAdmin, getAllOrders);
 router.post('/orders', authenticateToken, validate(orderSchema), createOrder);
 router.delete('/admin/orders/:id', authenticateToken, isAdmin, softDeleteOrder);
 
